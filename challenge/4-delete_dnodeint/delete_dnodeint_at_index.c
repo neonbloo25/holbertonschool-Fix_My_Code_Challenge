8f661612bef30e55e2b5072fe52ebcb6c0229588
neonbloo25 <theluckdemon@gmail.com>
2025-06-03 17:10:06 -0400

    re 4

--- challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -11,43 +11,29 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *saved_head;
-	dlistint_t *tmp;
-	unsigned int p;
+	dlistint_t *current = *head;
+	unsigned int i = 0;
 
-	if (*head == NULL)
-	{
+	if (!head || !(*head))
 		return (-1);
-	}
-	saved_head = *head;
-	p = 0;
-	while (p < index && *head != NULL)
+
+	while (current && i < index)
 	{
-		*head = (*head)->next;
-		p++;
+		current = current->next;
+		i++;
 	}
-	if (p != index)
-	{
-		*head = saved_head;
+
+	if (!current)
 		return (-1);
-	}
-	if (0 == index)
-	{
-		tmp = (*head)->next;
-		free(*head);
-		*head = tmp;
-		if (tmp != NULL)
-		{
-			tmp->prev = NULL;
-		}
-	}
+
+	if (current->prev)
+		current->prev->next = current->next;
 	else
-	{
-		(*head)->prev->prev = (*head)->prev;
-		free(*head);
-		if ((*head)->next)
-			(*head)->next->prev = (*head)->prev;
-		*head = saved_head;
-	}
+		*head = current->next;
+
+	if (current->next)
+		current->next->prev = current->prev;
+
+	free(current);
 	return (1);
 }
